e50a25da15b59853bb97fd52bf246e7052b7dfbe
beketamas <72067154+beketamas@users.noreply.github.com>
2023-11-15 10:46:08 +0100

    Add files via upload

---
+++ Lift.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelLift
+{
+    internal class Lift
+    {
+        DateTime idopont;
+        int kartyaSorszam;
+        int induloSorszam;
+        int celSorszam;
+
+        public Lift(string sor)
+        {
+            string[] tomb = sor.Split(' ');
+            this.idopont = DateTime.Parse(tomb[0]);
+            this.kartyaSorszam = int.Parse(tomb[1]);
+            this.induloSorszam = int.Parse(tomb[2]);
+            this.celSorszam = int.Parse(tomb[3]);
+        }
+
+        public DateTime Idopont => idopont;
+        public int KartyaSorszam  => kartyaSorszam;
+        public int InduloSorszam  => induloSorszam;
+        public int CelSorszam  => celSorszam;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿namespace HotelLift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Lift> listaLiftek = File.ReadAllLines("lift.txt").Select(x => new Lift(x)).ToList();
+            Console.WriteLine($"3. feladat: Összes lifthasználat: {listaLiftek.Count}");
+            Console.WriteLine($"4. feladat: Időszak: {listaLiftek.MinBy(x => x.Idopont)?.Idopont.ToShortDateString()} " +
+                $"- {listaLiftek.MaxBy(x => x.Idopont)?.Idopont.ToShortDateString()}");
+            Console.WriteLine($"5. feladat: Célszint max: {listaLiftek.MaxBy(x => x.CelSorszam)?.CelSorszam}");
+
+            //Console.WriteLine($"5. feladat: Célszint max: {lista.OrderBy(x => x.CelSorszam).Last().CelSorszam}");
+            //Console.WriteLine($"5. feladat: Célszint max: {lista.Select(x => x.CelSorszam).Max()}");
+
+            Console.WriteLine("6. feladat:");
+            int kartyaSorszam = 0;
+            int celSorszam = 0;
+            try
+            {
+                kartyaSorszam = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                kartyaSorszam = 5;
+            }
+            try
+            {
+                celSorszam = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                celSorszam = 5;
+            }
+            Console.WriteLine($"\tKártya szám: {kartyaSorszam}");
+            Console.WriteLine($"\tCélszint szám: {celSorszam}");
+            Console.WriteLine(listaLiftek.Exists(x => x.KartyaSorszam == kartyaSorszam && x.CelSorszam == celSorszam)
+                ? $"7. feladat: A(z) {kartyaSorszam}. kártyával utaztak a(z) {celSorszam}. emeletre"
+                : $"7. feladat: A(z) {kartyaSorszam}. kártyával nem utaztak a(z) {celSorszam}. emeletre");
+            /*string a = "";
+            string b = "nem";
+            Console.WriteLine($"7. feladat: A(z) {kartyaSorszam}. kártyával " +
+                $"{(lista.Any(x => x.KartyaSorszam == kartyaSorszam && x.CelSorszam == celSorszam) ? a : b)} utaztak a(z) {celSorszam}. emeletre");*/
+
+            File.WriteAllLines("statisztika.txt", listaLiftek.GroupBy(x => x.Idopont).Select(x => $"\t{x.Key} - {x.Count()}x"));
+        }
+    }
+}
